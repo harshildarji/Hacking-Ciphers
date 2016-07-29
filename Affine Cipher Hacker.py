f372f914063b8cd5f947bd360ed62af1ed9f6135
Harshil Darji <darjiharshil2994@gmail.com>
2016-07-29 14:42:21 +0530

    Initial

---
+++ Affine Cipher Hacker.py
@@ -0,0 +1,44 @@
+# Affine Cipher Hacker
+# http://inventwithpython.com/hacking (BSD Licensed)
+# Modified by: Harshil Darji (github.com/H-Darji)
+
+import pyperclip
+affineCipher = __import__('Affine Cipher')
+detectEnglish = __import__('Detecting English Programmatically')
+cryptoMath = __import__('Cryptomath Module')
+
+SILENT_MODE = False
+
+def main():
+    message = input('Encrypted message: ')
+    hackedMessage = hackAffine(message)
+
+    if hackedMessage != None:
+        print('Copying hacked message to clipboard...')
+        print('\nHacked message:\n', hackedMessage)
+        pyperclip.copy(hackedMessage)
+    else:
+        print('Failed to hack encrypton.')
+
+def hackAffine(message):
+    print('Hacking...\n[Press CTRL + C or CTRL + D to quit at any time]')
+    for key in range(len(affineCipher.SYMBOLS) ** 2):
+        keyA = affineCipher.getKeyParts(key)[0]
+        if cryptoMath.gcd(keyA, len(affineCipher.SYMBOLS)) != 1:
+            continue
+
+        decryptedText = affineCipher.decryptMessage(key, message)
+        if not SILENT_MODE:
+            print('Key #%s: %s...' % (key, decryptedText[:40]))
+
+        if detectEnglish.isEnglish(decryptedText):
+            print('\nPossible encryption hack:')
+            print('Key #%s: %s...' % (key, decryptedText[:40]))
+            print("Enter 'D' for done, or just press 'Enter' to continue hacking:")
+            response = input('> ')
+            if response.strip().upper().startswith('D'):
+                return decryptedText
+    return None
+
+if __name__ == '__main__':
+    main()
