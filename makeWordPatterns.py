f4b6084fb8abc69a5373580efdd795369d1b0cee
Harshil Darji <darjiharshil2994@gmail.com>
2016-08-07 20:53:11 +0530

    Initial

---
+++ makeWordPatterns.py
@@ -0,0 +1,43 @@
+# Make wordPatterns.py File
+# http://inventwithpython.com/hacking (BSD Licensed)
+
+import pprint, time
+
+def getWordPattern(word):
+    word = word.upper()
+    nextNum = 0
+    letterNums = {}
+    wordPattern = []
+
+    for letter in word:
+        if letter not in letterNums:
+            letterNums[letter] = str(nextNum)
+            nextNum += 1
+        wordPattern.append(letterNums[letter])
+    return '.'.join(wordPattern)
+
+def main():
+    startTime = time.time()
+    allPatterns = {}
+
+    fo = open('Dictionary.txt')
+    wordList = fo.read().split('\n')
+    fo.close()
+
+    for word in wordList:
+        pattern = getWordPattern(word)
+
+        if pattern not in allPatterns:
+            allPatterns[pattern] = [word]
+        else:
+            allPatterns[pattern].append(word)
+
+    fo = open('wordPatterns.py', 'w')
+    fo.write('allPatterns = ')
+    fo.write(pprint.pformat(allPatterns))
+    fo.close()
+    totalTime = round(time.time() - startTime, 2)
+    print('Done! [', totalTime, 'seconds ]')
+
+if __name__ == '__main__':
+    main()
