72a2ab3671689a91588ccb151c481d9011786cd9
Harshil Darji <darjiharshil2994@gmail.com>
2016-08-22 18:50:41 +0530

    initial

---
+++ prime_sieve.py
@@ -0,0 +1,32 @@
+# Prime Number Sieve (support module)
+# Source: http://inventwithpython.com/hacking (BSD Licensed)
+
+import math
+
+def isPrime(num):
+    if num < 2:
+        return False
+
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+
+    return True
+
+def primeSieve(sieveSize):
+    sieve = [True] * sieveSize
+    sieve[0] = False
+    sieve[1] = False
+
+    for i in range(2, int(math.sqrt(sieveSize)) + 1):
+        pointer = i * 2
+        while pointer < sieveSize:
+            sieve[pointer] = False
+            pointer += i
+
+    primes = []
+    for i in range(sieveSize):
+        if sieve[i] == True:
+            primes.append(i)
+
+    return primes
